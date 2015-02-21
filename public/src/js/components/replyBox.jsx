5c09f54852a0bdd7b141fd4852e5d31abaeaa7c9
Ryan Clark <ryan.clark@appsbroker.com>
2015-02-21 02:36:52 +0000

    Added state setting on input

--- public/src/js/components/replyBox.jsx
+++ public/src/js/components/replyBox.jsx
@@ -1,8 +1,18 @@
 var ReplyBox = React.createClass({
+	getInitialState: function () {
+		return {
+			value: ''
+		};
+	},
+	updateValue: function (e) {
+		this.setState({
+			value: e.target.value
+		});
+	},
 	render: function () {
 		return (
 			<div className="reply-box">
-				<input className="reply-box__input" placeholder="Type message to reply.." />
+				<input value={ this.state.value } onChange={ this.updateValue } className="reply-box__input" placeholder="Type message to reply.." />
 				<span className="reply-box__tip">
 					Press <span className="reply-box__tip__button">Enter</span> to send
 				</span>
